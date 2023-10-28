bbecdd153e2572d9ca9690da9853f8b9ead190a5
V M <o0o_v4mp1r3_o0o@outlook.com>
2023-10-27 21:24:30 -0500

    final

--- src/Database.js
+++ src/Database.js
@@ -4,6 +4,7 @@ function Database() {
   console.log({
     POSTGRES_URL: process.env.POSTGRES_URL,
     POSTGRES_URL_NON_POOLING: process.env.POSTGRES_URL_NON_POOLING,
+    env: process.env,
   });
   // const result =
   //   await sql`CREATE TABLE highScores (Score int, Name varchar(255));`;
